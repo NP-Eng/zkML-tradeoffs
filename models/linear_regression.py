15a29177848e26632162a29fa045bfccc33317c4
Cesar199999 <cesar.descalzo2@gmail.com>
2024-07-29 10:27:27 +0200

    Remove obsolete files. Add Plots

--- models/linear_regression.py
+++
@@ -1,9 +0,0 @@
-from torch import nn
-
-class LinearRegression(nn.Module):
-    def __init__(self, inputSize):
-        super(LinearRegression, self).__init__()
-        self.fc = nn.Linear(inputSize, 1)
-
-    def forward(self, x):
-        return self.fc(x)
